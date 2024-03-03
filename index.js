0b55264e88057b81135c5629c176efe82a1bd14e
Joseph1x <124495374+Joseph1x@users.noreply.github.com>
2024-03-03 01:44:56 +0000

    08899785

--- index.js
+++ index.js
@@ -1,39 +1,10 @@
-const fs = require('fs');
-const YouTube = require('youtube-sr').default;
-const ytdl = require('ytdl-core-discord');
 
-// Função para buscar vídeos
-async function searchAndDownload(query, limit, videoIndex) {
-    try {
-        const videos = await YouTube.search(query, { limit });
-        if (videos.length === 0) {
-            console.log('Nenhum vídeo encontrado.');
-            return;
-        }
-        if (videoIndex < 1 || videoIndex > videos.length) {
-            console.log('Índice do vídeo selecionado está fora do intervalo.');
-            return;
-        }
+const gemini = new Gemini(API_KEY);
 
-        const selectedVideo = videos[videoIndex - 1];
-        console.log(`Baixando vídeo: ${selectedVideo.title}`);
-
-        // Download do vídeo e salvando no arquivo
-        const readableStream = await ytdl(selectedVideo.url);
-        const outputFileName = `${selectedVideo.title}.mp4`;
-        const writableStream = fs.createWriteStream(outputFileName);
-        
-        readableStream.pipe(writableStream);
-        writableStream.on('finish', () => {
-            console.log('Download concluído:', outputFileName);
-        });
-        writableStream.on('error', (error) => {
-            console.error('Erro ao salvar o vídeo:', error);
-        });
-    } catch (error) {
-        console.error("Erro ao buscar e baixar o vídeo:", error);
-    }
-}
-
-// Exemplo de busca e download de vídeo
-searchAndDownload("MC Poze Vida Loka", 3, 1); // Pesquisa por vídeos relacionados ao MC Poze Vida Loka, limitando a 3 resultados, e baixa o primeiro vídeo encontrado
+console.log(
+    await gemini.ask("Oii, tudo bem??! Me forneça o link de um site de receitas super deliciosas", {
+        temperature: 0.5,
+        topP: 1,
+        topK: 10,
+    })
+);
